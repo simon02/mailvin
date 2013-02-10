01406ef999c7363b8b0574c49cb0f27d54a715cf
Simon Buelens <simonbuelens@gmail.com>
2013-02-10 17:01:53 +0100

    fix params

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -9,23 +9,23 @@ module Mailvin
 
       post '/' do
         puts 'schedule controller post received'
-        p params
+        puts
         data = Multimap.new
-        data[:from] = params['bcc']
+        data[:from] = params['message-headers']['bcc']
         data[:to] = params['from']
-        data[:subject] = "Follow up email sent to #{params['to']}"
-        data[:text] = "Sent auto follow up email to #{params['to']}"
-        data[:html] = "Sent auto follow up email to #{params['to']}"
-        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1"\
-          "@api.mailgun.net/v2/mailvin.mailgun.org/messages" , data
+        data[:subject] = "Follow up email sent to #{params['recipient']}"
+        data[:text] = "Sent auto follow up email to #{params['recipient']}"
+        data[:html] = "Sent auto follow up email to #{params['recipient']}"
+        p data
+        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
         data = Multimap.new
         data[:from] = params['from']
-        data[:to] = params['to']
+        data[:to] = params['recipient']
         data[:subject] = "RE: #{params['subject']}"
         data[:text] = 'follow up email'
         data[:html] = 'follow up email'
-        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1"\
-          "@api.mailgun.net/v2/mailvin.mailgun.org/messages" , data
+        p data
+        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
         'success'
       end
 
