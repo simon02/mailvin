420f845a02b2d6928a998d1a7184f593e2c92632
Simon Buelens <simonbuelens@gmail.com>
2013-02-10 17:33:42 +0100

    typo

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -11,20 +11,20 @@ module Mailvin
 
       post '/' do
         puts 'schedule controller post received'
-        headers = JSON.parse(params['message_headers'])
+        headers = JSON.parse(params['message-headers'])
         p headers
         to = headers['to']
         data = Multimap.new
         data[:from] = params['recipient']
         data[:to] = params['from']
-        data[:subject] = "Follow up email sent to #{params['recipient']}"
-        data[:text] = "Sent auto follow up email to #{params['recipient']}"
-        data[:html] = "Sent auto follow up email to #{params['recipient']}"
+        data[:subject] = "Follow up email sent to #{to}"
+        data[:text] = "Sent auto follow up email to #{to}"
+        data[:html] = "Sent auto follow up email to #{to}"
         p data
         RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
         data = Multimap.new
         data[:from] = params['from']
-        data[:to] = params['recipient']
+        data[:to] = to
         data[:subject] = "RE: #{params['subject']}"
         data[:text] = 'follow up email'
         data[:html] = 'follow up email'
