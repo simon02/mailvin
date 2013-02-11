f1c68f7a89b35b8af8d876a8fbb84e25bae54fcf
Simon Buelens <simonbuelens@gmail.com>
2013-02-11 18:06:50 +0100

    try to find the mistake

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -14,30 +14,35 @@ module Mailvin
         puts params['message-headers']
         headers = JSON.parse(params['message-headers'])
         # find to as a numbnut idiot w two left hands
-        # to = headers.each do |header|
-        #   key, value = header
-        #   if key == 'to'
-        #     return value
-        #   end
-        # end
-        to = 'simon@piesync.com'
-        data = Multimap.new
-        data[:from] = params['recipient']
-        data[:to] = params['from']
-        data[:subject] = "Follow up email sent to #{to}"
-        data[:text] = "Sent auto follow up email to #{to}"
-        data[:html] = "Sent auto follow up email to #{to}"
-        p data
-        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
-        data = Multimap.new
-        data[:from] = params['from']
-        data[:to] = to
-        data[:subject] = "RE: #{params['subject']}"
-        data[:text] = 'follow up email'
-        data[:html] = 'follow up email'
-        p data
-        RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
-        'success'
+        to = headers.find { |header|
+          key, value = header
+          if key == 'To'
+            return value
+          end
+        }.first()
+        p 'recipient'
+        p params['recipient']
+        p 'from'
+        p params['from']
+        p 'to'
+        p to
+        # data = Multimap.new
+        # data[:from] = params['recipient']
+        # data[:to] = params['from']
+        # data[:subject] = "Follow up email sent to #{to}"
+        # data[:text] = "Sent auto follow up email to #{to}"
+        # data[:html] = "Sent auto follow up email to #{to}"
+        # p data
+        # RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
+        # data = Multimap.new
+        # data[:from] = params['from']
+        # data[:to] = to
+        # data[:subject] = "RE: #{params['subject']}"
+        # data[:text] = 'follow up email'
+        # data[:html] = 'follow up email'
+        # p data
+        # RestClient.post "https://api:key-2gwv182xi8z31rlelm54i61qsnxtk9-1@api.mailgun.net/v2/mailvin.mailgun.org/messages", data
+        # 'success'
       end
 
     end
